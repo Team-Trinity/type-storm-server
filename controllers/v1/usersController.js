4b97763686c393a4d360b2309571262220cbdd7c
Nasif Ahmed <nasif2ahmed@gmail.com>
2024-02-27 01:38:59 +0600

    add average and top wpm endpoints

--- controllers/v1/usersController.js
+++ controllers/v1/usersController.js
@@ -10,6 +10,22 @@ const createUser = async (req, res) => {
     }
 };
 
+async function getAverageSpeed(req, res) {
+    // getting user email from query parameter
+    userService.getAverageSpeed(req.query.email).then((response) => {
+        return res.status(201).send(response)
+    })
+}
+
+async function getTopSpeed(req, res) {
+    // getting user email from query parameter
+    userService.getTopSpeed(req.query.email).then((response) => {
+        return res.status(201).send(response)
+    })
+}
+
 module.exports = {
-    createUser
+    createUser,
+    getAverageSpeed,
+    getTopSpeed
 };
--- services/v1/userService.js
+++ services/v1/userService.js
@@ -11,6 +11,51 @@ const createUser = async ({ name,email, role, lessonsTaken, wpmRecords,cpmRecord
     }
 };
 
+async function getAverageSpeed(email) {
+    try {
+
+        const data = await User.findOne({ email: email })
+
+        if (data) {
+            const sum = data.wpmRecords.reduce((accumulator, currentValue) => {
+                return accumulator + currentValue;
+            }, 0);
+            const average = sum / data.wpmRecords.length;
+            return { "average": average }
+        } else {
+            return { success: false, message: "user not found" }
+
+        }
+
+    } catch (error) {
+
+        console.error("Error: couldn't get average wpm", error);
+        return { success: false, message: "Failed to get average wpm" };
+    }
+}
+
+async function getTopSpeed(email) {
+    try {
+
+        const data = await User.findOne({ email: email })
+
+        if (data) {
+            const highestWPM = Math.max(...data.wpmRecords)
+            return { "highest": highestWPM }
+        } else {
+            return { success: false, message: "user not found" }
+
+        }
+
+    } catch (error) {
+
+        console.error("Error: couldn't get top wpm", error);
+        return { success: false, message: "Failed to get top wpm" };
+    }
+}
+
 module.exports = {
-    createUser
+    createUser,
+    getAverageSpeed,
+    getTopSpeed
 };
