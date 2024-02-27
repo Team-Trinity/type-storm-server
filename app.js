1ae3aba22fab80eba5855d33ab21f8d1644937b2
Md Saifur Rahman <saifurrshishir@gmail.com>
2024-02-27 12:34:06 +0600

    create vercel.json configuration file

--- app.js
+++ app.js
@@ -7,14 +7,21 @@ const PORT = process.env.PORT || 5000;
 const userRoutes = require('./routes/v1/userRoutes');
 
 // middlewares
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:3000",
+        "https://type-storm-one.vercel.app",
+        "https://type-storm-one.vercel.app",
+        "https://trinity-type-storm.vercel.app",
+    ]
+}));
 app.use(express.json());
 
 app.use("/api/v1/users", userRoutes);
 
 
 app.listen(PORT, () => {
-    mongoose.connect(`mongodb+srv://storm:${process.env.DB_PASS}@cluster0.7rpinix.mongodb.net/keyStormDB?retryWrites=true&w=majority&appName=Cluster0`)    
+    mongoose.connect(`mongodb+srv://storm:${process.env.DB_PASS}@cluster0.7rpinix.mongodb.net/keyStormDB?retryWrites=true&w=majority&appName=Cluster0`)
         .then(() => {
             console.log("Database connected...");
         })
